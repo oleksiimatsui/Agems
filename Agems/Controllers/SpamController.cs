80e07aa00449f79c68c3061ed0923fceb1770f84
Alex Matsui <oleksii.matsui@gmail.com>
2022-10-25 17:07:38 +0300

    scroll page update

---
+++ Agems/Controllers/SpamController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agems.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpamController : Controller
+    {
+        private readonly AgemsSoundsContext _context;
+
+        public SpamController(AgemsSoundsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Spam
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sound>>> GetSpam()
+        {
+            var _spam = _context.Sounds;
+            return await _spam.ToListAsync();
+        }
+    }
+}
---
+++ Agems/wwwroot/js/Scroll.js
@@ -0,0 +1,63 @@
+﻿
+document.addEventListener('scroll', function (e) {
+    let documentHeight = document.body.scrollHeight;
+    let currentScroll = window.scrollY + window.innerHeight;
+    if (currentScroll > documentHeight) {
+        addpage();
+    }
+})
+
+window.onload = function () {
+    addpage();
+}
+
+document.onscroll = function () {
+    let documentHeight = document.body.scrollHeight;
+    let currentScroll = window.scrollY + window.innerHeight;
+    if (currentScroll > documentHeight) {
+        addpage();
+    }
+};
+
+function displayData(data) {
+    const el = document.createElement('div');
+    el.classList.add('page');
+    data.forEach(sound => {
+        let cont = document.createElement('div');
+        cont.classList = "card border-warning mb-3";
+        cont.style.width = "70%";
+        el.appendChild(cont);
+        let header = document.createElement('div');
+        header.classList = "card-header";
+        header.textContent = sound.name;
+        cont.appendChild(header);
+        let cardbody = document.createElement('div');
+        cardbody.classList = "card-body";
+        cont.appendChild(cardbody);
+        let text = document.createElement('p');
+        text.classList = "card-text";
+        text.innerText = sound.about;
+        cardbody.appendChild(text);
+        let audio = document.createElement("audio");
+        audio.controls = 'controls';
+        let source = document.createElement("source");
+        source.src = "/sounds/" + sound.soundPath;
+        source.type = 'audio/mpeg';
+        audio.appendChild(source);
+        cardbody.appendChild(audio);
+    });
+    document.body.appendChild(el);
+}
+
+function display_error(error) {
+    alert(error);
+}
+
+var uri = 'api/Spam';
+
+function addpage() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => display_error(error));
+}
